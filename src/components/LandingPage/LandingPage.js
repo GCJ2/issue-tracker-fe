686be81192ad026b633c6612a696d9ce7ae3d425
Greg Johnston <gcjohnston91@gmail.com>
2020-08-04 19:21:18 -0400

    Register page styled

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Button, Checkbox, Form, Header} from 'semantic-ui-react'
-// import '../../../assets/pexels-photo-235985.jpeg'
 import './LandingPage.scss'
+import {Link} from "react-router-dom";
 
 const LandingPage = () => {
   return (
@@ -10,19 +10,18 @@ const LandingPage = () => {
         <Header as='h1'>Issue Tracker Login</Header>
         <div className='form'>
           <Form>
-            <Form.Field>
-              <label>Username</label>
+            <Form.Field className='form-field'>
               <input placeholder='Username'/>
             </Form.Field>
             <Form.Field>
-              <label>Password</label>
-              <input placeholder='Password'/>
+              <input placeholder='Password'
+                     type='password'/>
             </Form.Field>
             <Button className='submit-button' type='submit'>Sign In</Button>
           </Form>
         </div>
         <div className='bottom-content'>
-          <p>Create an account</p>
+          <p>Create an <Link to='register'>account </Link></p>
           <p>View Demo</p>
         </div>
       </div>
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import {Button, Form, Header} from "semantic-ui-react";
+import {Link} from "react-router-dom";
+
 
 const Register = () => {
   return (
-    <div>
-      Register
+    <div className='main-view'>
+      <div className='form-container'>
+        <Header as='h1'>Register</Header>
+        <div className='form'>
+          <Form>
+            <Form.Field className='form-field'>
+              <input placeholder='Username'/>
+            </Form.Field>
+            <Form.Field className='form-field'>
+              <input placeholder='Password'
+                     type='password'/>
+            </Form.Field>
+            <Form.Field>
+              {/*<label>Password Confirm</label>*/}
+              <input placeholder='Password Confirm'
+                     type='password'/>
+            </Form.Field>
+            <Button className='submit-button' type='submit'>Register</Button>
+          </Form>
+        </div>
+      </div>
     </div>
   );
 };
