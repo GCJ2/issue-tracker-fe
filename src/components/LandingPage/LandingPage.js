4d578c4635424b9ef851f00bce3b017840e255ef
Greg Johnston <gcjohnston91@gmail.com>
2020-08-04 18:39:44 -0400

    Landing page in progress

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import {Button, Checkbox, Form, Header} from 'semantic-ui-react'
-
+import './LandingPage.scss'
 
 const LandingPage = () => {
   return (
     <div className='main-view'>
       <div className='form-container'>
-        <Header as='h1'>Issue Tracker</Header>
+        <Header as='h1'>Issue Tracker Login</Header>
         <div className='form'>
           <Form>
             <Form.Field>
@@ -17,8 +17,12 @@ const LandingPage = () => {
               <label>Password</label>
               <input placeholder='Password'/>
             </Form.Field>
-            <Button type='submit'>Sign In</Button>
+            <Button className='submit-button' type='submit'>Sign In</Button>
           </Form>
+        </div>
+        <div className='bottom-content'>
+          <p>Create an account</p>
+          <p>View Demo</p>
         </div>
       </div>
     </div>
